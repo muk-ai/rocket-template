f99f4d0d2c8f13bd9b0610aa1959a66c98b62e1b
Kenta Mukai <r@muk.ai>
2020-12-13 14:41:26 +0000

    :recycle: rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 use rocket_contrib::serve::StaticFiles;
 
 #[get("/")]
@@ -8,8 +9,8 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+use rocket::request::{self, FromRequest, Request};
 use std::sync::atomic::{AtomicUsize, Ordering};
-use rocket::request::{self, Request, FromRequest};
 
 static ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
 struct RequestId(pub usize);
@@ -21,9 +22,9 @@ impl<'a, 'r> FromRequest<'a, 'r> for &'a RequestId {
         // The closure passed to `local_cache` will be executed at most once per
         // request: the first time the `RequestId` guard is used. If it is
         // requested again, `local_cache` will return the same value.
-        request::Outcome::Success(request.local_cache(|| {
-            RequestId(ID_COUNTER.fetch_add(1, Ordering::Relaxed))
-        }))
+        request::Outcome::Success(
+            request.local_cache(|| RequestId(ID_COUNTER.fetch_add(1, Ordering::Relaxed))),
+        )
     }
 }
 
@@ -36,13 +37,16 @@ fn count(id: &RequestId) -> String {
 fn params(id: Option<usize>) -> String {
     match id {
         Some(n) => format!("usize: {}", n),
-        None => "Not a usize".to_string()
+        None => "Not a usize".to_string(),
     }
 }
 
 fn main() {
     rocket::ignite()
         .mount("/", routes![index, count, params])
-        .mount("/public", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/public")))
+        .mount(
+            "/public",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/public")),
+        )
         .launch();
 }
