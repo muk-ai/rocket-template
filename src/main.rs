a3a5fcb2720c6c34e47a031d03e11f0832001f11
Kenta Mukai <r@muk.ai>
2021-01-09 14:22:00 +0000

    :recycle: move tasks

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,20 @@
 
 #[macro_use]
 extern crate rocket;
-use rocket_contrib::{json::Json, serve::StaticFiles};
-
 #[macro_use]
 extern crate diesel;
 
-mod task;
-use task::Task;
-
-mod schema;
-use schema::tasks;
+use rocket_contrib::serve::StaticFiles;
 
 mod connection;
+mod schema;
+mod task;
 
 mod handlers;
 use handlers::count;
 use handlers::hello_world;
 use handlers::params;
-
-use rocket::State;
-
-#[get("/tasks/<id>")]
-fn tasks_get(
-    id: i32,
-    pool: State<connection::PgPool>,
-) -> Result<Json<Task>, diesel::result::Error> {
-    use diesel::prelude::*;
-
-    let conn = pool.get().unwrap();
-    let query_result: QueryResult<Task> = tasks::table.find(id).get_result::<Task>(&conn);
-    query_result.map(|task| Json(task))
-}
+use handlers::tasks;
 
 fn main() {
     use dotenv::dotenv;
@@ -41,7 +24,7 @@ fn main() {
     rocket::ignite()
         .manage(connection::init_pool())
         .mount("/", routes![hello_world::index])
-        .mount("/", routes![count::count, params::params, tasks_get])
+        .mount("/", routes![count::count, params::params, tasks::tasks_get])
         .mount(
             "/public",
             StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/public")),
