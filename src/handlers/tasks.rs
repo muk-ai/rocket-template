e907a8a99c9c535fef0768487b80b89c33b5eb72
Kenta Mukai <r@muk.ai>
2021-01-10 16:31:55 +0000

    :sparkles: return not found when the record is not found

--- src/handlers/tasks.rs
+++ src/handlers/tasks.rs
@@ -1,4 +1,6 @@
 use diesel::prelude::*;
+use diesel::result::Error;
+use rocket::http::Status;
 use rocket::State;
 use rocket_contrib::json::Json;
 
@@ -7,11 +9,13 @@ use crate::schema::tasks;
 use crate::task::Task;
 
 #[get("/tasks/<id>")]
-pub fn tasks_get(
-    id: i32,
-    pool: State<connection::PgPool>,
-) -> Result<Json<Task>, diesel::result::Error> {
+pub fn tasks_get(id: i32, pool: State<connection::PgPool>) -> Result<Json<Task>, Status> {
     let conn = pool.get().unwrap();
     let query_result: QueryResult<Task> = tasks::table.find(id).get_result::<Task>(&conn);
-    query_result.map(|task| Json(task))
+    query_result
+        .map(|task| Json(task))
+        .map_err(|error| match error {
+            Error::NotFound => Status::NotFound,
+            _ => Status::InternalServerError,
+        })
 }
