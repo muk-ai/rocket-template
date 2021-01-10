91090c265193a808a5898a841c24b1396a18d11e
Kenta Mukai <r@muk.ai>
2021-01-10 12:50:40 +0000

    :recycle: move

--- src/handlers/tasks.rs
+++ src/handlers/tasks.rs
@@ -1,3 +1,4 @@
+use diesel::prelude::*;
 use rocket::State;
 use rocket_contrib::json::Json;
 
@@ -10,8 +11,6 @@ pub fn tasks_get(
     id: i32,
     pool: State<connection::PgPool>,
 ) -> Result<Json<Task>, diesel::result::Error> {
-    use diesel::prelude::*;
-
     let conn = pool.get().unwrap();
     let query_result: QueryResult<Task> = tasks::table.find(id).get_result::<Task>(&conn);
     query_result.map(|task| Json(task))
