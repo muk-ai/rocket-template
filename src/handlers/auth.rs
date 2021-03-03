3ff02297a5df534287d0a33499edb2703a2578ca
Kenta Mukai <r@muk.ai>
2021-03-03 15:45:14 +0000

    :sparkles: get Authorization header

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,6 +1,21 @@
 use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
+
+pub struct IdToken(String);
+
+impl<'a, 'r> FromRequest<'a, 'r> for IdToken {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        let token = request.headers().get_one("Authorization");
+        match token {
+            Some(token) => Outcome::Success(IdToken(token.to_string())),
+            None => Outcome::Failure((Status::Unauthorized, ())),
+        }
+    }
+}
 
 #[get("/auth/me")]
-pub fn get_auth_me() -> Status {
-    Status::Ok
+pub fn get_auth_me(id_token: IdToken) -> String {
+    id_token.0
 }
