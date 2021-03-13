56b0b5e8aefc27d2434136be0f41ccf374620e2e
Kenta Mukai <r@muk.ai>
2021-03-13 13:46:53 +0000

    :sparkles: find user from database

--- src/models/users/from_request.rs
+++ src/models/users/from_request.rs
@@ -1,7 +1,9 @@
 use rocket::http::Status;
 use rocket::request::{FromRequest, Outcome, Request};
 
+use super::repository;
 use super::User;
+use crate::connection::DbConn;
 use crate::firebase;
 use crate::id_token::IdToken;
 
@@ -9,12 +11,16 @@ impl<'a, 'r> FromRequest<'a, 'r> for User {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        let conn = request.guard::<DbConn>()?;
         let id_token = request.guard::<IdToken>()?;
 
         match firebase::auth::verify_id_token(id_token.0) {
             Ok(token_data) => {
                 let uid = token_data.claims.sub;
-                Outcome::Success(User { uid })
+                match repository::find(uid, conn) {
+                    Ok(user) => Outcome::Success(user),
+                    Err(_) => Outcome::Failure((Status::Unauthorized, ())),
+                }
             }
             Err(_) => Outcome::Failure((Status::Unauthorized, ())),
         }
