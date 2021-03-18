e0f24177327dbc140827b0c0a13f9df6942571c5
Kenta Mukai <r@muk.ai>
2021-03-18 15:15:06 +0000

    :recycle: use RangeFrom

--- src/id_token.rs
+++ src/id_token.rs
@@ -10,7 +10,7 @@ impl<'a, 'r> FromRequest<'a, 'r> for IdToken {
         let mut bearer_token: Option<String> = None;
         if let Some(authz_header) = request.headers().get_one("Authorization") {
             if authz_header.starts_with("Bearer ") {
-                let token = authz_header[6..authz_header.len()].trim();
+                let token = authz_header[6..].trim();
                 bearer_token = Some(token.to_string());
             }
         }
