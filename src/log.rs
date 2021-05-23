8f3e2185684d9018a396a34f9028262b8b713aab
Kenta Mukai <r@muk.ai>
2021-05-23 23:02:29 +0900

    :sparkles: log as json format

--- src/log.rs
+++ src/log.rs
@@ -2,6 +2,10 @@ use std::net::SocketAddr;
 
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::{Request, Response};
+use serde_json;
+use serde_json::json;
+
+use crate::config::CONFIG;
 
 pub struct AccessLogFairing;
 
@@ -17,9 +21,19 @@ impl Fairing for AccessLogFairing {
         if let Some(header) = request.headers().get_one("X-Cloud-Trace-Context") {
             let chunks: Vec<&str> = header.split(&['/', ';'][..]).collect();
             if let (Some(trace), Some(span)) = (chunks.get(0), chunks.get(1)) {
-                println!("{}", header);
-                println!("{}", trace);
-                println!("{}", span);
+                let trace = format!("projects/{}/traces/{}", &CONFIG.gcp_project_id, trace);
+                let log = json! {
+                    {
+                        "severity": "INFO",
+                        "message": format!("hello, cloud logging. header is {}", header),
+                        "logging.googleapis.com/trace": trace,
+                        "logging.googleapis.com/spanId": span
+                    }
+                };
+
+                if let Ok(log) = serde_json::to_string(&log) {
+                    println!("{}", log);
+                }
             }
         }
 
