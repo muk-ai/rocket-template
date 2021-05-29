af1b81ea0e8481a6fb7a5be3ca3ae4d2106df731
Kenta Mukai <r@muk.ai>
2021-05-29 22:05:13 +0900

    :sparkles: create TraceContext
    - get TraceContext from X-Cloud-Trace-Context header
    - save it to thread local cache

--- src/log.rs
+++ src/log.rs
@@ -1,5 +1,7 @@
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::{Request, Response};
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
+use rocket::Response;
 use serde_json;
 use serde_json::json;
 
@@ -34,5 +36,60 @@ impl Fairing for AccessLogFairing {
                 }
             }
         }
+    }
+}
+
+pub struct TraceContext {
+    trace: String,
+    span_id: String,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for &'a TraceContext {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        let trace_context = request.local_cache(|| {
+            if let Some(header) = request.headers().get_one("X-Cloud-Trace-Context") {
+                let chunks: Vec<&str> = header.split(&['/', ';'][..]).collect();
+                if let (Some(trace), Some(span)) = (chunks.get(0), chunks.get(1)) {
+                    let trace = format!("projects/{}/traces/{}", &CONFIG.gcp_project_id, trace);
+                    return Some(TraceContext {
+                        trace: trace,
+                        span_id: span.to_string(),
+                    });
+                }
+            }
+            return None;
+        });
+        match trace_context {
+            Some(trace) => Outcome::Success(trace),
+            None => Outcome::Failure((Status::InternalServerError, ())),
+        }
+    }
+}
+
+pub fn write_log(message: &str, context: Option<&TraceContext>) {
+    let log = match context {
+        Some(context) => {
+            json! {
+                {
+                    "severity": "INFO",
+                    "message": message,
+                    "logging.googleapis.com/trace": context.trace,
+                    "logging.googleapis.com/spanId": context.span_id
+                }
+            }
+        }
+        None => {
+            json! {
+                {
+                    "severity": "INFO",
+                    "message": message,
+                }
+            }
+        }
+    };
+    if let Ok(log) = serde_json::to_string(&log) {
+        println!("{}", log);
     }
 }
