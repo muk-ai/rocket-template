1fd846f7c968e7d73c933304ea243e7acdc82012
Kenta Mukai <r@muk.ai>
2021-07-17 23:16:30 +0900

    :gear: set some attributes

--- src/handlers/cookies.rs
+++ src/handlers/cookies.rs
@@ -1,8 +1,15 @@
-use rocket::http::{Cookie, CookieJar};
+use rocket::http::{Cookie, CookieJar, SameSite};
+use time::{Duration, OffsetDateTime};
 
 #[get("/cookies")]
 pub fn cookies(cookies: &CookieJar<'_>) -> Option<String> {
-    cookies.add(Cookie::new("first", "value"));
+    let one_month_later = OffsetDateTime::now_utc() + Duration::weeks(4);
+    let cookie = Cookie::build("first", "value")
+        .http_only(true)
+        .same_site(SameSite::Lax)
+        .expires(one_month_later)
+        .finish();
+    cookies.add(cookie);
     cookies
         .get("message")
         .map(|crumb| format!("Message: {}", crumb.value()))
