e65fe502b8fd0c4c21267de68cc9b1511712e75d
Kenta Mukai <r@muk.ai>
2021-07-17 22:15:47 +0900

    :sparkles: add cookie

--- src/handlers/cookies.rs
+++ src/handlers/cookies.rs
@@ -1,7 +1,8 @@
-use rocket::http::CookieJar;
+use rocket::http::{Cookie, CookieJar};
 
 #[get("/cookies")]
 pub fn cookies(cookies: &CookieJar<'_>) -> Option<String> {
+    cookies.add(Cookie::new("first", "value"));
     cookies
         .get("message")
         .map(|crumb| format!("Message: {}", crumb.value()))
