4dd8eecdc5a119ccfb8944649d86f6ff90cab430
Kenta Mukai <r@muk.ai>
2021-07-18 21:55:12 +0900

    :sparkles: add private cookie

--- src/handlers/cookies.rs
+++ src/handlers/cookies.rs
@@ -4,18 +4,30 @@ use time::{Duration, OffsetDateTime};
 #[get("/set_cookies")]
 pub fn set_cookies(cookies: &CookieJar<'_>) -> &'static str {
     let one_month_later = OffsetDateTime::now_utc() + Duration::weeks(4);
-    let cookie = Cookie::build("message", "value")
+    let cookie1 = Cookie::build("message", "value")
         .http_only(true)
         .same_site(SameSite::Lax)
         .expires(one_month_later)
         .finish();
-    cookies.add(cookie);
+    cookies.add(cookie1);
+    let cookie2 = Cookie::build("private", "private value")
+        .http_only(true)
+        .same_site(SameSite::Lax)
+        .expires(one_month_later)
+        .finish();
+    cookies.add_private(cookie2);
     "ok"
 }
 
 #[get("/cookies")]
 pub fn cookies(cookies: &CookieJar<'_>) -> Option<String> {
-    cookies
-        .get("message")
-        .map(|crumb| format!("Message: {}", crumb.value()))
+    let message = cookies.get("message").map(|crumb| crumb.value().to_owned());
+    let private = cookies
+        .get_private("private")
+        .map(|crumb| crumb.value().to_owned());
+    if let (Some(message), Some(private)) = (message, private) {
+        Some(format!("message: {}, private: {}", message, private))
+    } else {
+        None
+    }
 }
