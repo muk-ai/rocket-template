bcbee6eafc6e241fece4e01f4010b106d47fbf02
Kenta Mukai <r@muk.ai>
2021-11-06 21:22:50 +0900

    :gear: change datatype from uuid to integer

--- migrations/2021-11-03-120929_create_deleted_users/up.sql
+++ migrations/2021-11-03-120929_create_deleted_users/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE deleted_users (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id SERIAL PRIMARY KEY,
   user_id uuid NOT NULL,
   user_firebase_uid VARCHAR(128) NOT NULL,
   user_deleted_at timestamptz NOT NULL DEFAULT current_timestamp,
